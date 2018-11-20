aaaa667e306fb5d1234478819b48f1e151df8b84
kazola <joaquim.oller@gmail.com>
2018-11-20 18:09:47 -0500

    BLE tests coverage up to 100%. They need a bit clean up but they seem in good shape.

--- tests/test_logger_controller_ble.py
+++ tests/test_logger_controller_ble.py
@@ -4,7 +4,8 @@ from unittest import TestCase
 from mat.logger_controller_ble import (
     LoggerControllerBLE,
     Delegate,
-    LCBLEException
+    LCBLEException,
+    XModemException
 )
 
 
@@ -19,7 +20,7 @@ class FakeOutStream():
         pass
 
 
-class FakeOutStreamTellWentWrong():
+class FakeOutStreamTellIsWrong(FakeOutStream):
     def tell(self):
         return 0
 
@@ -30,7 +31,7 @@ class FakeData:
         self.valHandle = index
 
     def write(self, data, withResponse=False):
-        print("mocked data: write")
+        pass
 
 
 class FakeDataIndexable:
@@ -77,6 +78,9 @@ class FakePeripheral():
         print("mocked peripheral: waitForNotifications")
         return True
 
+    def disconnect(self):
+        print("mocked peripheral: disconnect")
+
 
 class FakePeripheralCmdTimeout(FakePeripheral):
     def waitForNotifications(self, value):
@@ -89,7 +93,7 @@ class TestLoggerControllerBLE(TestCase):
     # test constructor, patch with stub class, FakePeripheral() gets called
     @patch("bluepy.btle.Peripheral", FakePeripheral)
     def test_lc_ble_constructor(self):
-        assert LoggerControllerBLE("ff:ff:ff:ff:ff:ff")
+        assert LoggerControllerBLE("ff:ff:ff:ff:ff:ff") is not None
 
     # test mock usage, patch with mock class, MagicMock() gets called
     @patch("bluepy.btle.Peripheral")
@@ -110,8 +114,8 @@ class TestLoggerControllerBLE(TestCase):
         d = Delegate()
         d.xmodem_mode = True
         d.sentC = False
-        d.handleNotification(None, b'\n\rnGET 00\r\n')
-        assert d.buffer
+        d.handleNotification(None, b'\n\rGET 00\r\n')
+        assert len(d.buffer) == 10
 
     # test for receiving file while in xmodem format
     def test_handleNotification_xmodem_yes_sentC(self):
@@ -119,7 +123,7 @@ class TestLoggerControllerBLE(TestCase):
         d.xmodem_mode = True
         d.sentC = True
         d.handleNotification(None, b'\x02\x01\xfe\x00\xff')
-        assert d.xmodem_buffer
+        assert len(d.xmodem_buffer) == 5
 
     # test for parsing while in ascii mode
     def test_read_line_no_read_buffer(self):
@@ -132,10 +136,11 @@ class TestLoggerControllerBLE(TestCase):
         assert "".join(d.read_line()) == "any_ascii_string"
 
     # test for open method
-    @patch("bluepy.btle.Peripheral")
-    def test_open(self, my_mock):
-        LoggerControllerBLE("ff:ff:ff:ff:ff:ff").open()
-        pass
+    def test_open_close(self):
+        with _peripheral_patch():
+            lc_ble = LoggerControllerBLE("ff:ff:ff:ff:ff:ff")
+            lc_ble.open()
+            assert lc_ble.close() == "ok"
 
     # test for a command which requires no answer
     def test_command_no_answer_required(self):
@@ -179,7 +184,7 @@ class TestLoggerControllerBLE(TestCase):
             lc_ble = LoggerControllerBLE("ff:ff:ff:ff:ff:ff")
             assert lc_ble.control_command("data_X") is "CMDAOKMLDP"
 
-    # test for writing characteristics, used by command()
+    # test for writing characteristics, used by command(), must do nothing
     def test_write(self):
         with _peripheral_patch():
             lc_ble = LoggerControllerBLE("ff:ff:ff:ff:ff:ff")
@@ -201,20 +206,55 @@ class TestLoggerControllerBLE(TestCase):
     def test_list_files_answer_empty(self):
         with _command_patch(True, "\x04"):
             lc_ble = LoggerControllerBLE("ff:ff:ff:ff:ff:ff")
-            lc_ble.list_files()
+            assert lc_ble.list_files() == []
 
     # test for command "DIR" when logger answers a populated file list
     def test_list_files_answer_ok(self):
         rl_answers = Mock(side_effect=["one.h\t12", "two.dat\t2345", "\x04"])
         with _command_patch_dir(True, rl_answers):
             lc_ble = LoggerControllerBLE("ff:ff:ff:ff:ff:ff")
-            lc_ble.list_files()
+            assert lc_ble.list_files() == [('one.h', 12), ('two.dat', 2345)]
 
     # test for command "GET" when logger sends a file correctly
     def test_get_files_answer_ok(self):
         with _command_patch_get(True, "GET 00"):
             lc_ble = LoggerControllerBLE("ff:ff:ff:ff:ff:ff")
-            lc_ble.get_file("which_file", 12345, FakeOutStream())
+            assert lc_ble.get_file("want_file", 12345, FakeOutStream()) is None
+
+    # test for command "GET" when logger timeouts while sending
+    def test_get_files_answer_timeout(self):
+        with _command_patch_get(False, None):
+            lc_ble = LoggerControllerBLE("ff:ff:ff:ff:ff:ff")
+            self.assertRaises(LCBLEException, lc_ble.get_file,
+                              "want_file", 12345, FakeOutStream())
+
+    # test for command "GET" when logger sends a file too-small
+    def test_get_files_answer_too_small(self):
+        with _command_patch_get(True, "GET 00"):
+            lc_ble = LoggerControllerBLE("ff:ff:ff:ff:ff:ff")
+            self.assertRaises(XModemException, lc_ble.get_file,
+                              "want_file", 12345, FakeOutStreamTellIsWrong())
+
+    # test for xmodem getc() function
+    def test_getc(self):
+        with _peripheral_patch():
+            lc_ble = LoggerControllerBLE("ff:ff:ff:ff:ff:ff")
+            lc_ble.delegate.xmodem_buffer = b'\x02\x01\xfe'
+            assert lc_ble.getc(2) == b'\x02\x01'
+
+    # test for xmodem putc() function, ascii mode, sent "C"
+    def test_putc_not_sentC(self):
+        with _write_characteristic_patch():
+            lc_ble = LoggerControllerBLE("ff:ff:ff:ff:ff:ff")
+            lc_ble.delegate.sentC = False
+            assert lc_ble.putc("C") == 1
+
+    # test for xmodem putc() function, xmodem mode
+    def test_putc_yes_sentC(self):
+        with _write_characteristic_patch():
+            lc_ble = LoggerControllerBLE("ff:ff:ff:ff:ff:ff")
+            lc_ble.delegate.sentC = True
+            assert lc_ble.putc(b'\x06') == 1
 
 
 # vars useful for context managers
@@ -252,12 +292,9 @@ def _command_patch_dir(rv_in_waiting, rl_method):
 
 @contextmanager
 def _command_patch_get(rv_in_waiting, rv_read_line):
-    with patch(peripheral_class, FakePeripheral):
-        with patch(lc_ble_write_method):
-            with patch(d_in_waiting_property, return_value=rv_in_waiting):
-                with patch(d_read_line_method, return_value=rv_read_line):
-                    with patch(xmodem_recv_method):
-                        yield
+    with _command_patch(rv_in_waiting, rv_read_line):
+        with patch(xmodem_recv_method):
+            yield
 
 
 @contextmanager
@@ -265,3 +302,9 @@ def _command_patch_timeout():
     with patch(peripheral_class, FakePeripheralCmdTimeout):
         with patch(lc_ble_write_method):
             yield
+
+
+@contextmanager
+def _write_characteristic_patch():
+    with _command_patch(None, None):
+        yield
